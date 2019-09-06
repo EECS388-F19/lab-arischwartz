3c81eab2eb5d6c2af049387119691f4cee81c7ca
Ari Schwartz <ariflynn@beyster1620p29.engin.umich.edu>
2019-09-06 16:17:19 -0400

    did lab

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+from random import randrange
+print("Ari")
+x = randrange(0,99)
+y = randrange(0,99)
+print(x)
+print(y)
+add = x + y
+print("Sum = " + str(add))
+avg = add / 2
+print("Average = " + str(avg))
+
+
+
